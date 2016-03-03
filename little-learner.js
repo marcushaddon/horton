8143c503f1caaaca77bf814e0236745b66760581
John Marcus Haddon II <marcushaddon@Johns-MacBook-Pro-2.local>
2016-03-02 18:47:18 -0600

    deleted everything and started over

--- little-learner.js
+++ little-learner.js
@@ -1,99 +1,162 @@
-// define additional class of Follower with word value and count
-// 
-
-
-function Entry(word) {
+function Follower(word) {
   this.word = word;
-  this.followers = [];
-  this.addFollower = function(value) {
-    if (this.followers.indexOf(value) === -1) {
-      this.followers.push(value);
-    } else {
+  this.timesObserved = 1;
+  this.observe = function() {
+    this.timesObserved += 1;
+  }
+}
+
+function vocabWord(word) {
+  this.word = word;
+  this.timesObserved = 0; // hmm...
+
+  this.followers = {
+    list: [],
+    search: function(word) {
+      //if word is in my followers, return the Follower for that word
+      
+      for (index in this.list) {
+        var currentEntry = this.list[index];
+        if (currentEntry.word === word) {
+          return currentEntry;
+        }  
+      }
       return false;
     }
   };
-  this.chooseNext = function() {
-    var choice = Math.floor(Math.random() * this.followers.length);
-    return this.followers[choice];
-  }
+
+  this.recordFollower = function(word) {
+    var exists = this.followers.search(word);
+
+    if (exists) {
+      exists.observe();
+     
+     } else {
+      this.followers.list.push(new Follower(word));
+      console.log("I learned a new association!");
+     }
+
+     this.timesObserved += 1;
+  };
+
+  // 
+  this.chooseFollower = function() {
+    // this is probably a terrible way to do this, will fix later
+    var weightedArray = [];
+    var list = this.followers.list;
+
+    // add each follower to weightedArray as many times as it has been observed
+    for (index in this.followers.list) {
+      for (var i = 0; i < list[index].timesObserved; i++) {
+        weightedArray.push(list[index]);
+      }
+    }
+
+    // evenly distributed random choice from weighted array
+    var choice = Math.floor(Math.random() * weightedArray.length);
+    // console.log(weightedArray);
+    return weightedArray[choice];
+
+
+  };
 }
 
-var lib = {
-  tree: [],
-  getRandEntry: function() {
-    var choice = Math.floor(Math.random() * this.tree.length);
-    return this.tree[choice];
+var horton = {
+  vocabulary: {
+    list: [],
+    addWord: function(word) {
+      this.list.push(new vocabWord(word));
+    },
+
+    search: function(word) {
+      // if word is in my vocabulary, return the vocabWord for that word
+      // this is WAY not DRY, will refactor later
+      for (index in this.list) {
+        var currentEntry = this.list[index];
+        if (currentEntry.word === word) {
+          return currentEntry;
+        }  
+      }
+      return false;
+    }
   },
+
+  getRandomWord: function() {
+    var choice = Math.floor(Math.random() * this.vocabulary.length);
+    return this.vocabulary[choice];
+  },
+
   read: function(text) {
+    var vocab = this.vocabulary;
     var daria = text.toLowerCase().replace(/[^a-z|\s]/gi, '');
     var textArray = daria.split(' ');
-    if (this.tree.length === 0) {
-      this.tree.push(new Entry(textArray[0]));
-    }
-    
     console.log(textArray);
     
-    //do this for each word in the textArray
-    for (var i = 0, length = textArray.length; i < length; i ++) {
-      
-      var isNew = true;
-      var currentWord = textArray[i];
-      var nextWord = textArray[i + 1];
-      
-      //now loop through current tree to see if we already have this word
-      for (var x = 0, len = this.tree.length; x < len; x++) {
-        var currentEntry = this.tree[x];
-        
-        //if we have it, add the next word as a follower
-        if (currentEntry.word == currentWord) {
-          currentEntry.addFollower(nextWord);
-          console.log('New follower!');
-          isNew = false;
-          break;
-        }
-        
-      }
-      
-      //if we made it through all that and dont have an entry for currentWord, add it and its follower
-      if (isNew) {
-        this.tree.push(new Entry(currentWord));
-        this.tree[this.tree.length - 1].addFollower(nextWord);
-      }
+    // if this is our first word, then go ahead and add it!
+    if (vocab.list.length === 0) {
+      vocab.addWord(textArray[0]);
+      // need to learn first assoc here
     }
 
+    // now lets read each word
+    for (var i = 0, length = textArray.length; i < length; i++) {
+
+      var currentWord = textArray[i];
+      var followingWord = textArray[i + 1]; 
+
+      // check to see if we know this word
+      var wordThatExists = vocab.search(currentWord);
+
+      // if we dont know it, learn it and learn its association!
+      if (!wordThatExists) {
+        this.vocabulary.addWord(currentWord);
+        var lastWord = this.vocabulary.list[this.vocabulary.list.length - 1];
+        lastWord.recordFollower(followingWord);
+      } else if (wordThatExists) {
+        wordThatExists.recordFollower(followingWord);
+      }
+    }
   },
 
-  write: function(wordCount) {
-    //new empty poem
+  write: function(wordCount, seed) {
+    
+    // blank page
     var poem = [];
 
-    //choose a first word
-    var firstWord = this.getRandEntry();
+    // open to suggestion but not nessecary
+    var firstWord = this.vocabulary.search(seed) || this.getRandomWord();
     poem.push(firstWord);
 
-    //write
-    for (i = 1; i < wordCount; i++) {
-      poem[i] = chooseNext(poem[i - 1]);
+    // write
+    for (var i = 1; i < wordCount; i++) {
+      poem[i] = poem[i - 1].chooseFollower(); // NOT WORKING
     }
 
-    //translate
-    for (word in poem) {
-      poem[word] = poem[word].word;
-    }
-
-    //print
-    return poem.join(' ');
+    console.log(poem);
   }
-};
 
-function chooseNext(entry) {
-  var choice = Math.floor(Math.random() * entry.followers.length);
-  var word = entry.followers[choice];
 
-  for (var i = 0, length = lib.tree.length; i < length; i++) {
-    if (lib.tree[i].word == word) {
-      return lib.tree[i];
-    }
-  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
